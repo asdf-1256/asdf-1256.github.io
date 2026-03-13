42508c5982036a53ce586c868b18109bfc359f02
윤재영 <22112106@yu.ac.kr>
2026-03-13 14:50:17 +0900

    TOEIC, PASSPORT

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1769941812',resources: [
+    cacheName: 'chirpy-1773380882',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
